da13623f0ad7d6c8d776a9997cd3a071a232261a
Miraj <mirajholmes@gmail.com>
2019-08-04 09:56:01 +0530

    minor fixes

--- truffle-config.js
+++ truffle-config.js
@@ -6,9 +6,12 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     development: {
+        // provider: function() {
+        //     return new Web3.providers.HttpProvider("https://testnet2.matic.network")
+        // },
         host: 'localhost',
         port: 8545,
-        network_id: '*'
+        network_id: '*',
     }
   }
 };
